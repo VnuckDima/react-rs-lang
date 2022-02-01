28664bd69153b611d93161d0f129f44ed253ef81
DimaVnuk <vnuchok311994@hotmail.com>
2022-02-01 20:27:25 +0300

    feat: added AudioCall.tsx and Sprint.tsx

---
+++ src/components/games/audio-call/AudioCall.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AudioCall() {
+  return <div>Audio Call</div>;
+}
+
+export default AudioCall;
---
+++ src/components/games/sprint/Sprint.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Sprint() {
+  return <div>Sprint</div>;
+}
+
+export default Sprint;
