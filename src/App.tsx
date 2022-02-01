3d14f079eea73ace8c0912d4447d3eb08734311c
DimaVnuk <vnuchok311994@hotmail.com>
2022-02-01 20:30:02 +0300

    feat: added Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
 import Header from './components/header/Header';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+
     </div>
   );
 }
