23561cce6d69bc23638eb04d3a5668e12d67468f
DimaVnuk <vnuchok311994@hotmail.com>
2022-02-02 20:38:25 +0300

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AudioCall from './components/games/audio-call/AudioCall';
+import Sprint from './components/games/sprint/Sprint';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="container">
       <Header />
-
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="audio-call" element={<AudioCall />} />
+	  <Route path="sprint" element={<Sprint />} />
+      </Routes>
     </div>
   );
 }
