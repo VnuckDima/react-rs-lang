c7fd93c025f7c69754cbb856d8e31e4a33cb67e4
DimaVnuk <vnuchok311994@hotmail.com>
2022-02-03 17:09:30 +0300

    feat: added components textbooks, navigation

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,23 @@ import AudioCall from './components/games/audio-call/AudioCall';
 import Sprint from './components/games/sprint/Sprint';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Navigation from './components/navigation/Navigation';
+import Statistics from './components/statistics/Statistics';
+import Textbook from './components/textbook/Textbook';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <div className="root">
+        <Navigation />
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="audio-call" element={<AudioCall />} />
         <Route path="sprint" element={<Sprint />} />
+        <Route path="textbook" element={<Textbook />} />
+        <Route path="statistics" element={<Statistics />} />
       </Routes>
     </div>
   );
